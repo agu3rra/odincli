3c7d8e03b616f61ae5d325b623bc5a8dde3353f2
agu3rra <agu3rra@pm.me>
2024-05-18 14:50:26 -0300

    Updated fabric submodule

---
+++ fabric-update.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Navigate to the submodule directory
+cd fabric
+
+# Fetch and pull the latest changes
+git fetch --depth 1
+git pull origin main
+
+# Remove unwanted files
+find . -mindepth 1 -maxdepth 1 ! -name 'patterns' -exec rm -rf {} +
+
+# Navigate back to the main project directory
+cd ..
+
+# Add and commit changes
+git add fabric
+git commit -m "Updated fabric submodule"
